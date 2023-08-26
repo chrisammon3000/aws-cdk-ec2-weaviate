4dcaee65a191b0ecad046b501223297e2583a547
Gregory Lindsey <gclindsey@gmail.com>
2023-08-26 13:41:17 -0700

    clone repo on boot

--- src/config.sh
+++ src/config.sh
@@ -21,38 +21,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 apt-get update -y
-apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin git -y
 usermod -a -G docker ubuntu
-curl -o /home/ubuntu/docker-compose.yaml "https://configuration.weaviate.io/v2/docker-compose/docker-compose.yml?generative_cohere=false&generative_openai=false&generative_palm=false&gpu_support=false&media_type=text&modules=modules&ner_module=false&qna_module=false&ref2vec_centroid=false&runtime=docker-compose&spellcheck_module=false&sum_module=false&text_module=text2vec-transformers&transformers_model=sentence-transformers-multi-qa-MiniLM-L6-cos-v1&weaviate_version=v1.19.8"
-sleep 1
-awk '
-  /^  weaviate:$/ {
-    print
-    print "    restart: always"
-    print "    volumes:"
-    print "      - /data/weaviate:/var/lib/weaviate"
-    while(getline && $0 !~ /^  /);
-    if ($0 ~ /^  /) {
-      print
-    }
-    next
-  }
-  /^  t2v-transformers:$/ {
-    print
-    print "    restart: always"
-    while(getline && $0 !~ /^  /);
-    if ($0 ~ /^  /) {
-      print
-    }
-    next
-  }
-  /CLUSTER_HOSTNAME: '\''node1'\''/ {
-    print
-    print "      AUTOSCHEMA_ENABLED: '\''false'\''"
-    next
-  }
-  /restart: on-failure:0/ {
-    next
-  }
-  1' /home/ubuntu/docker-compose.yaml > /home/ubuntu/docker-compose-temp.yaml && mv /home/ubuntu/docker-compose-temp.yaml /home/ubuntu/docker-compose.yaml
-cd /home/ubuntu && docker compose up -d
+export REPOSITORY_URL=https://github.com/chrisammon3000/aws-cdk-ec2-weaviate.git
+git clone $REPOSITORY_URL /home/ubuntu/app
+cd /home/ubuntu/app && docker compose up -d
