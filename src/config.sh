06c0bb49ae1a836d66939fa7d23894bb0acf029d
Gregory Lindsey <gclindsey@gmail.com>
2023-08-26 20:15:58 -0700

    deploy app

--- src/config.sh
+++ src/config.sh
@@ -9,3 +9,7 @@ chkconfig docker on
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose version
+mkdir -p /opt/app /opt/data/weaviate_data
+chown -R ec2-user:ec2-user /opt/app /opt/data
+git clone https://github.com/chrisammon3000/aws-cdk-ec2-weaviate.git /opt/app
+cd /opt/app && docker-compose up -d
